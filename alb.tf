0b5f3f720abeeabfe5cb611930cabc396a0e0c38
Kaushal Kumar <107171506+kaushal-90@users.noreply.github.com>
2022-09-26 15:29:53 +0530

    Add files via upload

---
+++ alb.tf
@@ -0,0 +1,47 @@
+# Creating additional load balancer
+resource "aws_lb" "external-alb" {
+    name = "External-LB"
+    internal = false
+    load_balancer_type = "application"
+    security_groups = [ aws_security_group.demosg.id]
+    subnets = [aws_subnet.public_sub1.id, aws_subnet.public_sub2.id]
+
+}
+
+resource "aws_lb_target_group" "target-elb" {
+    name = "alb-TG"
+    port = 80
+    protocol = "http"
+    vpc_id = aws_vpc.demo_vpc.id
+
+}
+
+resource "aws_lb_target_group_attachment" "attachment" {
+    target_group_arn = aws_lb_target_group.external-alb.arn
+    target_id = aws_instance.demoinstance.id
+    port = 80
+    depends_on = [
+      aws_instance.demoinstance
+    ]
+  
+}
+
+resource "aws_lb_target_group_attachment" "attachment" {
+    target_group_arn = aws_lb_target_group.external-alb.arn
+    target_id = aws_instance.demoinstance1.id
+    port = 80
+    depends_on = [
+      aws_instance.demoinstance1
+    ]
+}
+
+resource "aws_lb_listener" "external-elb" {
+    load_balancer_arn = aws_lb.external-alb.arn
+    port = 80
+    protocol = "http"
+    defauldefault_action {
+      type ="forward"
+      target_group_arn = aws_lb_target_group.external-alb.arn
+    }  
+}
+
